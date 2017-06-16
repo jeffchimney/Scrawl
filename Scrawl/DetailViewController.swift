a0436f873324b6877ec939ad7e461151ac37dd23
Jeff Chimney <jeff.chimney@avssystems.ca>
2017-06-16 16:32:31 -0700

    Initial commit

---
+++ Scrawl/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Scrawl
+//
+//  Created by Jeff Chimney on 2017-06-16.
+//  Copyright © 2017 Jeff Chimney. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let thisScrawl = scrawl {
+            if let label = detailDescriptionLabel {
+                label.text = thisScrawl.name!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var scrawl: Project? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ Scrawl/TableViewCells/ProjectCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProjectCell.swift
+//  Scrawl
+//
+//  Created by Jeff Chimney on 2017-06-16.
+//  Copyright © 2017 Jeff Chimney. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProjectCell: UITableViewCell {
+    @IBOutlet weak var projectTitleTextField: UITextField!
+}
